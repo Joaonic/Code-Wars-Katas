8829eec75b32fe091db5a4db873be4ba74319a89
Joaonic <55551413+Joaonic@users.noreply.github.com>
2020-07-30 21:59:53 -0300

    added katas in C and JS

---
+++ 8 kyu/Function_1__hello_world/Function_1__hello_world.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int greet(){
+    return "hello world!";
+}
---
+++ 8 kyu/Is_n_divisible_by_x_and_y/Is_n_divisible_by_x_and_y.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+bool isDivisible(int n, int x, int y) {
+
+    if(n % x == 0){
+        if(n % y == 0){
+            return true;
+          }
+    else;
+        return false;
+    }
+}
